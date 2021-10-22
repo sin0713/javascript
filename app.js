d6c36fa589ddc5c6f81feaca2e341d1c729cc9f0
sin0713 <narisin1124@gmail.com>
2021-10-23 06:05:57 +0900

    add

--- app.js
+++ app.js
@@ -1,35 +1,70 @@
 
-let hello = "Hello, World";
-alert(hello);
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)) {
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-let init1 = 1;
-alert(init1);
+}
 
-alert(4 + 3);
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
 
-let str1 = "Hello";
-let str2 = "World";
-alert(str1 + str2);
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
 
-let orange = 100;
-let apple = 120;
+// 結果を表示する
+if(user_hand != null) {
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 
- if(orange < apple){
-   alert('みかんの値段がリンゴより安い');
- } else if (orange == apple){
-   alert('みかんとリンゴが同じ値段')
- } else {
-   alert('みかんの値段がリンゴより高い')
- }
+} else {
+  alert('またチャレンジしてね');
+}
 
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
 
-  let i;
-  let num = 0;
-
-  for(i = 1; i < 11; i++){
-    num = num + i;
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
   }
 
-  alert('1から10まで足し算した結果は' + num + 'です');
+  return hand_name;
+}
 
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
 
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
---
+++ sample.js
@@ -0,0 +1,11 @@
+
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
