23be5d96a8df56727de47a601e8830dfde52e476
sin0713 <narisin1124@gmail.com>
2021-10-22 12:49:26 +0900

    [finish]javascript2章

---
+++ app.js
@@ -0,0 +1,35 @@
+
+let hello = "Hello, World";
+alert(hello);
+
+let init1 = 1;
+alert(init1);
+
+alert(4 + 3);
+
+let str1 = "Hello";
+let str2 = "World";
+alert(str1 + str2);
+
+let orange = 100;
+let apple = 120;
+
+ if(orange < apple){
+   alert('みかんの値段がリンゴより安い');
+ } else if (orange == apple){
+   alert('みかんとリンゴが同じ値段')
+ } else {
+   alert('みかんの値段がリンゴより高い')
+ }
+
+
+  let i;
+  let num = 0;
+
+  for(i = 1; i < 11; i++){
+    num = num + i;
+  }
+
+  alert('1から10まで足し算した結果は' + num + 'です');
+
+
